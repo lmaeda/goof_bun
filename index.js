239c18c1a624a9252faf652085bc98b06ce7229f
lucmaeda <luc.maeda@snyk.io>
2024-10-22 10:02:57 +0900

    quick test on bun and snyk

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from Bun!');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
